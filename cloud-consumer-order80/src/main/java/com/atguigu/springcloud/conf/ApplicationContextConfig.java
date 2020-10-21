e024b6128802dda9b7770f855fd5d444ab31752a
wujing <1046696449@qq.com>
2020-10-21 15:58:57 +0800

    4.工程重构

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/conf/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/conf/ApplicationContextConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * @Author: wujing
- * @Description: spring的配置类
- * @Date: 2020/10/21 14:15
+ * @Description: spring配置类
+ * @Date: 2020/10/21 15:36
  * @Version: 1.0
  */
+
 @Configuration
 public class ApplicationContextConfig {
 
@@ -17,6 +18,5 @@ public class ApplicationContextConfig {
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
-
 
 }
