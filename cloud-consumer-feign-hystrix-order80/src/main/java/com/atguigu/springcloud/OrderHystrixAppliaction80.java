8b22aa539251f272e0de3e8c0565c454e8543aab
wujing <1046696449@qq.com>
2020-10-26 17:58:42 +0800

    看热闹不嫌弃事大，OrderFeignApplication80新建加入

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/OrderHystrixAppliaction80.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @Author: wujing
+ * @Description: feign-hystrix
+ * @Date: 2020/10/26 14:13
+ * @Version: 1.0
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@FeignClient
+public class OrderHystrixAppliaction80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixAppliaction80.class, args);
+    }
+}
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: wujing
+ * @Description:
+ * @Date: 2020/10/26 17:43
+ * @Version: 1.0
+ */
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+
+
+}
+
