02798219096ff20a7aeadd80e347be0f580d2fa6
wujing <1046696449@qq.com>
2020-11-20 17:36:20 +0800

    9003和9004  服务新建

---
+++ cloudalibaba-sentinel-provider-payment9003/src/main/java/com/atguigu/springcloud/PaymentMain9003.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: wujing
+ * @Description: sentinel整合 ribbon + openFeign + fallback
+ * @Date: 2020/11/20 11:07
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9003
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9003.class, args);
+    }
+}
